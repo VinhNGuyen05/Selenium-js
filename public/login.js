0e8357b6e5bd186b37dec159154fe89a0aaa7fe6
VinhNGuyen05 <vinhnguyen50501@gmail.com>
2022-06-19 16:21:31 +0700

    finish project test

--- public/login.js
+++ public/login.js
@@ -1,15 +1,19 @@
 function validate() {
+    var map = new Map();
+    map.set("admin", "1");
+    map.set("user", "1");
+    map.set("test", "1");
+
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
-    if (username != "admin" && password != "1") {
-        window.location = "./results/userpass.html";
-    } else {
-        if (username != "admin") {
-            window.location = "./results/user.html";
-        } else if (password != "1") {
-            window.location = "./results/pass.html"; // Redirecting to other page.
+
+    if (map.has(username)) {
+        if (map.get(username) === password) {
+            window.location = "./results/welcome.html";
         } else {
-            window.location = "./results/success.html";
+            window.location = "./results/pass.html";
         }
+    } else {
+        window.location = "./results/user.html";
     }
 }
--- tests/test.js
+++ tests/test.js
@@ -1,30 +1,28 @@
 const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
 
-async function example() {
-
-    var username = "admin";
-    var password = "1";
-
+async function check(username, password, num) {
     //To wait for browser to build and launch properly
     let driver = await new Builder().forBrowser("chrome").build();
-
-
-    //To fetch http://google.com from the browser with our code.
-    await driver.get("file:///D:/FPT Course/5 - Summer 2022/SWT301/Selenium-js/logins/login.html");
-
+    //To fetch login from the browser.
+    await driver.get("https://selenium-js.web.app");
     //To send a search query by passing the value in searchString.
     await driver.findElement(By.id("username")).sendKeys(username, Key.RETURN);
     await driver.findElement(By.id("password")).sendKeys(password, Key.RETURN);
     await driver.findElement(By.id("submit")).click();
-
     //Verify the page title and princdt it
     var title = await driver.getTitle();
-    console.log('Log is:', title);
-
-    //It is always a safe practice to quit the browser after execution
+    console.log('Status of test case ' + num + ' is:', title);
     await driver.quit();
-
 }
 
-example()
+function testCase() {
+    check("admin", "1", 1);
+    check("user", "1", 2);
+    check("userasd", "2eqe", 3);
+    check("test", "2", 4);
+    check("test234", "1", 5);
+    check("test", "1", 6);
+}
+
+testCase()
