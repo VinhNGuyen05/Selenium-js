f4308c33d0ef71a7746f331fd94b08dedff3b751
VinhNGuyen05 <vinhnguyen50501@gmail.com>
2022-06-26 15:18:39 +0700

    change console log

--- tests/test.js
+++ tests/test.js
@@ -1,36 +1,39 @@
 const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
 
-// const testCase = [
-//     { username: "test", password: "123" },
-//     { username: "test", password: "132" },
-//     { username: "selenium", password: "123" },
-//     { username: "admin", password: "admin" }
-// ];
+const testCase = [
+    { username: "test", password: "123" },
+    { username: "test", password: "132" },
+    { username: "selenium", password: "123" },
+    { username: "admin", password: "admin" },
+    { username: "user", password: "123" },
+    { username: "group4", password: "test" },
+];
+//6 test case
 
-async function check(username, password) {
+async function checkAccount(item) {
     var result;
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("https://selenium-js.web.app");
-    await driver.findElement(By.id("username")).sendKeys(username, Key.RETURN);
-    await driver.findElement(By.id("password")).sendKeys(password, Key.RETURN);
+    await driver.findElement(By.id("username")).sendKeys(item.username, Key.RETURN);
+    await driver.findElement(By.id("password")).sendKeys(item.password, Key.RETURN);
     await driver.findElement(By.id("submit")).click();
     var title = await driver.getTitle();
+    console.log('username: ' + item.username + ' | password: ' + item.password);
     if (title.localeCompare("Welcome") === 0) {
-        result = " : test valid";
+        // result = " : pass";
+        console.log('\x1b[32m%s\x1b[0m', 'pass');
     } else {
-        result = " : test invalid";
+        console.log('\x1b[31m%s\x1b[0m', 'fail');
     }
-    console.log('---- username: ' + username + ' | password: ' + password + result);
     await driver.quit();
 }
 
-async function testCase() {
+const test = async() => {
     console.clear();
-    await check("admin", "123");
-    await check("user", "132");
-    await check("selenium", "123");
-    await check("admin", "admin");
-}
+    for (item of testCase) {
+        await checkAccount(item);
+    }
+};
 
-testCase()
+test();
