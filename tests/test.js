43c5a5692287ea586d6a10f11ecd96cbd44cd824
VinhNGuyen05 <vinhnguyen50501@gmail.com>
2022-06-27 20:19:47 +0700

    finish project

--- tests/test.js
+++ tests/test.js
@@ -2,17 +2,15 @@ const { By, Key, Builder } = require("selenium-webdriver");
 require("chromedriver");
 
 const testCase = [
-    { username: "test", password: "123" },
-    { username: "test", password: "132" },
-    { username: "selenium", password: "123" },
+    { username: "test", password: "1" },
+    { username: "admin", password: "1312" },
+    { username: "user", password: "1" },
     { username: "admin", password: "admin" },
-    { username: "user", password: "123" },
-    { username: "group4", password: "test" },
+    { username: "admin", password: "1" },
 ];
-//6 test case
+//5 test case
 
 async function checkAccount(item) {
-    var result;
     let driver = await new Builder().forBrowser("chrome").build();
     await driver.get("https://selenium-js.web.app");
     await driver.findElement(By.id("username")).sendKeys(item.username, Key.RETURN);
@@ -21,7 +19,6 @@ async function checkAccount(item) {
     var title = await driver.getTitle();
     console.log('username: ' + item.username + ' | password: ' + item.password);
     if (title.localeCompare("Welcome") === 0) {
-        // result = " : pass";
         console.log('\x1b[32m%s\x1b[0m', 'pass');
     } else {
         console.log('\x1b[31m%s\x1b[0m', 'fail');
